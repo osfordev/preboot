61ebaad839f42c73fc56680fbfa2b671dd5a6bec
Max Anurin <maxim.anurin@zxteam.net>
2023-05-01 20:02:48 +0300

    Finalize arm32v7 image

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -4,11 +4,11 @@
 if [ -x /preboot/build.sh ]; then
 	cd /preboot
 	exec /preboot/build.sh "$@"
-else
-	echo
-	echo "A script /preboot/build.sh not found. Entering shell ..."
-	echo
-	sleep 3
 fi
+
+echo
+echo "A script /preboot/build.sh not found. Entering shell ..."
+echo
+sleep 3
 
 exec /bin/bash "$@"
